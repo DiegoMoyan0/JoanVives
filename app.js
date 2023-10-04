05cf9433ea535c9b754ccad4c8f0222d8a9e3814
DiegoMoyan0 <diegomoyano02@gmail.com>
2023-10-03 21:02:52 -0300

    project Joan

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+const transporter = require("./helpers/mailer")
+
+app.use(express.static('public'));
+
+app.get("/login/:email/code", async function (req, res){
+    const {email} = req.params
+    const result = await transporter.sendMail({
+    from: 'diegomoyano02@gmail.com', 
+    to: email, 
+    subject: "Hello ✔", 
+    body: "Hello world?", 
+
+});
+console.log({result});
+res.status(200).json({ok: true, message: "Codigo enviado con exito!"})
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+});
+
+app.listen(port, () => {
+    console.log(`Servidor web iniciado en el puerto ${port}`);
+})
+
+// tls: {
+//     rejectUnauthorized: false // Ignorar errores de certificado
+// }
+// Ruta para enviar un correo
+// app.get('/enviar-correo', (req, res) => {
+//     const mailOptions = {
+//         from: 'diegomoyano02@gmail.com',
+//         to: 'diegoalejomoyano@gmail.com',
+//         subject: 'Asunto del correo',
+//         text: 'Contenido del correo',
+//     };
+
+//     transporter.sendMail(mailOptions, (error, info) => {
+//         if (error) {
+//             console.error(error);
+//             res.send('Error al enviar el correo');
+//         } else {
+//             console.log('Correo enviado: ' + info.response);
+//             res.sendFile(path.join(__dirname, '/views/acces.html'));
+//         }
+//     });
+// })
---
+++ public/index.js
@@ -0,0 +1,74 @@
+  // Función para animar el número gradualmente
+  function animateNumber(targetElement, targetNumber) {
+    const duration = 2000; // Duración de la animación en milisegundos
+    const steps = 100; // Cantidad de pasos en la animación
+    const increment = targetNumber / steps;
+    let currentNumber = 0;
+
+    function updateNumber() {
+      currentNumber += increment;
+      targetElement.textContent = Math.floor(currentNumber);
+
+      if (currentNumber < targetNumber) {
+        requestAnimationFrame(updateNumber);
+      } else {
+        targetElement.textContent = "+" + targetNumber;
+      }
+    }
+
+    updateNumber();
+  }
+
+  // Función para verificar si un elemento está en la vista del usuario
+  function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  // Función para manejar el evento scroll
+  function handleScroll() {
+    const number1 = document.getElementById("number-1");
+    const number2 = document.getElementById("number-2");
+
+    if (isElementInViewport(number1) && !number1.dataset.animated) {
+      animateNumber(number1, 500);
+      number1.dataset.animated = true; // Marcar como animado
+    }
+
+    if (isElementInViewport(number2) && !number2.dataset.animated) {
+      animateNumber(number2, 100);
+      number2.dataset.animated = true; // Marcar como animado
+    }
+  }
+
+
+
+  // Escuchar el evento scroll
+  window.addEventListener("scroll", handleScroll);
+
+  // Función para mostrar el pop-up
+function showPopup() {
+  const popup = document.getElementById("popup");
+  popup.style.display = "block";
+}
+
+// Función para cerrar el pop-up
+function closePopup() {
+  const popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
+
+// Escuchar el evento de clic en el checkbox
+const checkbox = document.getElementById("termsCheckbox");
+checkbox.addEventListener("click", function () {
+  if (checkbox.checked) {
+    showPopup();
+  } else {
+    closePopup();
+  }
+});
